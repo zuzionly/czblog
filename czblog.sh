7b76b3a6c19214595668052461f86d5b920979c6
Nicholas <zuzionly@gmail.com>
2012-11-11 01:44:18 +0800

    update shell command

--- czblog.sh
+++ czblog.sh
@@ -1,19 +1,21 @@
 . /simple/venv/bin/activate
 cd /simple
-process=`ps aux | grep gunicorn | grep -v grep`;
-if [ "$process" == "" ]; then
+process1=`ps -A | grep gunicorn | grep -v grep`;
+if [ "$process1" == "" ]; then
 	echo "starting gunicorn"
 	/simple/venv/bin/gunicorn -w 4 -p /tmp/gunicorn.pid czblog:app &
 else
+	echo "$process1"
 	echo "gunicorn is running"
 fi
-process=`ps aux | grep watchmedo | grep -v grep`;
-if [ "$process" == "" ]; then
+process2=`ps -A | grep watchmedo | grep -v grep`;
+if [ "$process2" == "" ]; then
 	echo "starting watchdog"
 	nohup watchmedo shell-command \
          --patterns="*.py" \
 	 --recursive \
          --command='kill -HUP `cat /tmp/gunicorn.pid`' /simple
 else
+	echo "$process2"
 	echo "watch dog is running"
 fi
